9d3835067238ef21a79891e03b2b4e7d9ae193f9
Nahyun-K <rnjsskgus990313@gmail.com>
2022-02-19 17:41:19 +0900

    jpa

--- hello-spring/src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
+++ hello-spring/src/main/java/hello/hellospring/repository/JdbcTemplateMemberRepository.java
@@ -1,4 +1,5 @@
 package hello.hellospring.repository;
+
 import hello.hellospring.domain.Member;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -27,24 +28,20 @@ public class JdbcTemplateMemberRepository implements MemberRepository {
         member.setId(key.longValue());
         return member;
     }
-
     @Override
     public Optional<Member> findById(Long id) {
-        List<Member> result = jdbcTemplate.query("select * from member where id  = ?", memberRowMapper(), id);
+        List<Member> result = jdbcTemplate.query("select * from member where id = ?", memberRowMapper(), id);
         return result.stream().findAny();
     }
-
+    @Override
+    public List<Member> findAll() {
+        return jdbcTemplate.query("select * from member", memberRowMapper());
+    }
     @Override
     public Optional<Member> findByName(String name) {
         List<Member> result = jdbcTemplate.query("select * from member where name = ?", memberRowMapper(), name);
         return result.stream().findAny();
     }
-
-    @Override
-    public List<Member> findAll() {
-        return jdbcTemplate.query("select * from member", memberRowMapper());
-    }
-
     private RowMapper<Member> memberRowMapper() {
         return (rs, rowNum) -> {
             Member member = new Member();
